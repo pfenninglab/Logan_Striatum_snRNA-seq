db3962421de0da7a2534cde9e2c6c778605665b5
badoi <badoi@me.com>
2021-12-07 10:32:30 -0500

    make plot of per sample QC

---
+++ figures/exploratory/preprocess_snRNA-seq_reads/step2_plot_gene_UMAP_ViolinPlots_BU_striatum.R
@@ -0,0 +1,153 @@
+## conda activate r4
+## packages for data table processing 
+library(here)
+library(tidyverse)
+library(RColorBrewer)
+
+## main Seurat package snRNA-seq pacakges
+library(Seurat)
+library(SeuratDisk)
+library(future)
+
+## gene expression heatmap plots
+library(Nebulosa)
+
+ss <- function(x, pattern, slot = 1, ...) { 
+  sapply(strsplit(x = x, split = pattern, ...), '[', slot) }
+options(stringsAsFactors = F)
+options(repr.plot.width=11, repr.plot.height=8.5)
+
+PLOTDIR='figures/exploratory/preprocess_snRNA-seq_reads'
+
+###################################
+# 0) pre-set colors and cell types 
+subtypes = c('D1-Matrix', 'D2-Matrix', 'D1/D2-Hybrid',  'D1-Striosome', 'D2-Striosome')
+subtypes_col = brewer.pal(n = length(subtypes)+1, name = 'Paired') [-3]
+names(subtypes_col) = subtypes
+
+othertypes = c('Interneurons', 'Astrocytes', 'Endothelial', 'Microglia', 
+               'Mural/Fibroblast', 'Oligos', 'Oligos_Pre')
+othertypes_col = brewer.pal(n = length(othertypes), name = 'Dark2')
+names(othertypes_col) = othertypes
+
+
+##################################################
+# 1) load in full dataset cell type labels for plot
+
+## read in Logan BU snRNA dataset to label transfer
+obj_merged = here('data/tidy_data/Seurat_projects', 
+                  "BU_Run1_Striatum_filtered_SCT_SeuratObj_N4.h5Seurat") %>% LoadH5Seurat() 
+obj_merged[['group']] = with(obj_merged[[]], ifelse(celltype2 %in% subtypes, 'MSN', 'Other'))
+obj_merged$group = relevel(factor(obj_merged$group), ref = 'Other')
+obj_merged$celltype2 = factor(obj_merged$celltype2 , c(subtypes, othertypes))
+DefaultAssay(obj_merged) = 'RNA'
+
+## Neuron vs. Glia markers
+markerGenes1 <- c('RBFOX3', 'LHX6', 'AQP4', 'CX3CR1', 'PDGFRA', 'MOG' )
+pdf(here(PLOTDIR, 'plots', 'BU_Run1_Striatum_UMAP_all.allMarkers.pdf'), width = 7.25, height = 4)
+p1 = plot_density(obj_merged, features = markerGenes1,  reduction = "umap") +
+  plot_layout(nrow = 2, guides = "auto") &
+  theme_classic(base_size = 7) & theme(plot.title = element_text(size = 10))
+p1
+dev.off()
+
+pdf(here(PLOTDIR, 'plots', 'BU_Run1_Striatum_Viol_all.allMarkers.pdf'), width = 7.25, height = 5)
+VlnPlot(obj_merged, features =c(markerGenes1), slot = "data", cols = c(subtypes_col, othertypes_col)) & 
+  theme(legend.position = 'none', axis.title.x=element_blank()) 
+dev.off()
+
+## MSN subtypes
+markMSN1 = c('Drd1','Tac1','Reln') %>% toupper()# D1 markers
+markMSN2 = c('Drd2','Adora2a','Penk')%>% toupper() # D2 markers
+markMSN3 = c('Foxp2', 'Rxfp1', 'Casz1')%>% toupper() # D1/2H markers
+
+pdf(here(PLOTDIR, 'plots', 'BU_Run1_Striatum_UMAP_all.MSNclusterMarkers.pdf'), width = 7.25, height = 4)
+p2 = plot_density(obj_merged, slot = 'data', features = markMSN1,  reduction = "umap", joint= T) &
+  theme_classic(base_size = 7) & theme(plot.title = element_text(size = 8))
+p3 = plot_density(obj_merged, slot = 'data', features = markMSN2,  reduction = "umap", joint= T) &
+  theme_classic(base_size = 7) & theme(plot.title = element_text(size = 8))
+p4 = plot_density(obj_merged, slot = 'data', features = markMSN3,  reduction = "umap", joint= T) &
+  theme_classic(base_size = 7) & theme(plot.title = element_text(size = 8))
+p2 + plot_layout(nrow = 1) & theme(legend.position = 'bottom')
+p3 + plot_layout(nrow = 1) & theme(legend.position = 'bottom')
+p4 + plot_layout(nrow = 1) & theme(legend.position = 'bottom')
+dev.off()
+
+pdf(here(PLOTDIR, 'plots', 'BU_Run1_Striatum_Viol_all.MSNclusterMarkers.pdf'), width = 7.25, height = 5)
+VlnPlot(obj_merged, features =c(markMSN1), slot = "data", ncol = 3,
+        group.by = 'celltype1', cols = c(subtypes_col, othertypes_col)) & 
+  theme(legend.position = 'none', axis.title.x=element_blank()) 
+VlnPlot(obj_merged, features =c(markMSN2), slot = "data", ncol = 3,
+        group.by = 'celltype1', cols = c(subtypes_col, othertypes_col)) & 
+  theme(legend.position = 'none', axis.title.x=element_blank()) 
+VlnPlot(obj_merged, features =c(markMSN3), slot = "data", ncol = 3,
+        group.by = 'celltype1', cols = c(subtypes_col, othertypes_col)) & 
+  theme(legend.position = 'none', axis.title.x=element_blank()) 
+dev.off()
+
+
+## MSN compartments
+markMSN4 = c('STXBP6', 'SEMA3E', 'EPHA4', 'GDA') # Matrix markers
+markMSN5 =c( 'PDYN', 'OPRM1', 'KHDRBS3', 'KCNIP1') # Striosome markers
+
+pdf(here(PLOTDIR, 'plots', 'BU_Run1_Striatum_UMAP_all.MSNcompMarkers.pdf'), width = 7.25, height = 2)
+p5 = plot_density(obj_merged, slot = 'data', features = markMSN4,  reduction = "umap") 
+p6 = plot_density(obj_merged, slot = 'data', features = markMSN5,  reduction = "umap", pal = 'inferno')
+p5 + plot_layout(nrow = 1) & theme(legend.position = 'bottom')  &
+  theme_classic(base_size = 7) & theme(plot.title = element_text(size = 8)) 
+p6 + plot_layout(nrow = 1) & theme(legend.position = 'bottom')  &
+  theme_classic(base_size = 7) & theme(plot.title = element_text(size = 8))
+dev.off()
+
+# you can plot raw counts as well
+markOPR1 = c('OPRD1', 'OPRM1', 'OPRK1') # opioid receptor genes
+markOPR2 = c( 'PENK', 'PDYN') # endorphin peptide genes
+obj_merged$celltype1 = factor(obj_merged$celltype1, levels = c('MSNs', othertypes)) 
+Idents(obj_merged) = 'celltype1'
+obj_merged$DSM.IV.SUD  = ifelse(obj_merged$Dur.OUD > 0, 'OUD', 'CTL')
+
+pdf(here(PLOTDIR, 'plots', 'BU_Run1_Striatum_Viol_all.opioids.pdf'), width = 7.25, height = 5)
+VlnPlot(obj_merged, features =c(markOPR1, markOPR2), ncol = 4, slot = "data",
+        group.by = 'celltype1', cols = c(subtypes_col, othertypes_col)) & 
+  theme(legend.position = 'none', axis.title.x=element_blank()) 
+dev.off()
+
+pdf(here(PLOTDIR, 'plots', 'BU_Run1_Striatum_Viol_all.opioidsByDxSUD.pdf'), width = 7.25, height = 4)
+VlnPlot(obj_merged, features =c(markOPR1), slot = "data", ncol = 4, group.by = 'celltype1',
+        split.by = 'DSM.IV.OUD', cols = c('gray', 'red'), split.plot = TRUE) & 
+  theme(axis.title.x=element_blank()) 
+dev.off()
+
+
+
+
+#####################################
+# 2) plot genes at MSN subtype level
+## read in Logan BU snRNA dataset to label transfer
+obj_msn = here('data/tidy_data/Seurat_projects', 
+               "BU_Run1_Striatum_subsetMSN_SCT_SeuratObj_N4.h5Seurat") %>% 
+  LoadH5Seurat()
+DefaultAssay(obj_msn) = 'RNA'
+obj_msn$celltype2 = factor(obj_msn$celltype2 , c(subtypes, othertypes))
+Idents(obj_msn) = 'celltype2'
+
+pdf(here(PLOTDIR, 'plots', 'BU_Run1_Striatum_Viol_msn.MSNcompMarkers.pdf'), width = 7.25, height = 5)
+VlnPlot(obj_msn, features =c(markMSN4, markMSN5), slot = "data", ncol = 4,
+        group.by = 'celltype2', cols = c(subtypes_col, othertypes_col)) & 
+  theme(legend.position = 'none', axis.title.x=element_blank()) 
+dev.off()
+
+
+pdf(here(PLOTDIR, 'plots', 'BU_Run1_Striatum_Viol_msn.opioids.pdf'), width = 7.25, height = 5)
+VlnPlot(obj_msn, features =c(markOPR1, markOPR2), slot = "data", cols = subtypes_col) & 
+  theme(legend.position = 'none', axis.title.x=element_blank())
+dev.off()
+
+
+pdf(here(PLOTDIR, 'plots', 'BU_Run1_Striatum_Viol_msn.opioidsByDxSUD.pdf'), width = 7.25, height = 4)
+VlnPlot(obj_msn, features =c(markOPR1), slot = "data", ncol = 4,
+        split.by = 'DSM.IV.SUD', cols = c('gray', 'red'), split.plot = TRUE) & 
+  theme(legend.position = 'none', axis.title.x=element_blank()) 
+dev.off()
+
+
---
+++ figures/exploratory/preprocess_snRNA-seq_reads/step3_plot_UMAP_integratedMacaqueLabels.R
@@ -0,0 +1,112 @@
+## conda activate r4
+## packages for data table processing 
+library(here)
+library(tidyverse)
+library(RColorBrewer)
+
+## main Seurat package snRNA-seq pacakges
+library(Seurat)
+library(SeuratDisk)
+library(future)
+
+ss <- function(x, pattern, slot = 1, ...) { 
+  sapply(strsplit(x = x, split = pattern, ...), '[', slot) }
+options(stringsAsFactors = F)
+options(repr.plot.width=11, repr.plot.height=8.5)
+
+PLOTDIR='figures/exploratory/preprocess_snRNA-seq_reads'
+
+###################################
+# 0) pre-set colors and cell types 
+subtypes = c('D1-Matrix', 'D2-Matrix', 'D1/D2-Hybrid',  'D1-Striosome', 'D2-Striosome')
+subtypes_col = brewer.pal(n = length(subtypes)+1, name = 'Paired') [-3]
+names(subtypes_col) = subtypes
+
+othertypes = c('Interneurons', 'Astrocytes', 'Endothelial', 'Microglia', 
+               'Mural/Fibroblast', 'Oligos', 'Oligos_Pre')
+othertypes_col = brewer.pal(n = length(othertypes), name = 'Dark2')
+names(othertypes_col) = othertypes
+
+##################################################
+# 1) load in full dataset cell type labels for plot
+
+## read in Logan BU snRNA dataset to label transfer
+obj_merged = here('data/tidy_data/Seurat_projects', 
+   "BU_Run1_Striatum_filtered_SCT_SeuratObj_N4.h5Seurat") %>% LoadH5Seurat() 
+obj_merged[['group']] = with(obj_merged[[]], ifelse(celltype2 %in% subtypes, 'MSN', 'Other'))
+obj_merged$group = relevel(factor(obj_merged$group), ref = 'Other')
+
+pdf(here(PLOTDIR, 'plots', 'BU_Run1_Striatum_UMAP_all.ident.pdf'), width = 7.25, height = 4)
+Idents(obj_merged) = obj_merged$orig.ident
+DimPlot(object = obj_merged, reduction = "umap", split.by = 'orig.ident', 
+             cols = brewer.pal(n = 4, name = 'Set2') ) +
+  guides(color = guide_legend(nrow = 1, override.aes= list(size = 3))) + 
+  theme(legend.position = 'bottom')
+dev.off()
+
+pdf(here(PLOTDIR, 'plots', 'BU_Run1_Striatum_UMAP_all.clusters.pdf'), width = 7.25, height = 4)
+DimPlot(object = obj_merged, reduction = "umap", 
+        group.by = 'seurat_clusters',  split.by = 'orig.ident') +
+  guides(color = guide_legend(nrow = 1, override.aes= list(size = 3)))+ 
+  theme(legend.position = 'bottom')
+dev.off()
+
+pdf(here(PLOTDIR, 'plots', 'BU_Run1_Striatum_UMAP_all.macaqueLabels.pdf'), width = 7.25, height = 4)
+DimPlot(object = obj_merged, reduction = "umap",  split.by = 'orig.ident', 
+        group.by = 'celltype2', cols = c(subtypes_col, othertypes_col)) +
+  guides(color = guide_legend(nrow = 3, override.aes= list(size = 3))) + 
+  theme(legend.position = 'bottom')
+dev.off()
+
+pdf(here(PLOTDIR, 'plots', 'BU_Run1_Striatum_UMAP_all.macaqueLabels2.pdf'), width = 7.25, height = 4)
+DimPlot(object = obj_merged, reduction = "umap", group.by = 'celltype2', 
+        shape.by = 'celltype2', pt.size = .75) +
+  scale_colour_manual(name = "Cell type", labels = names(c(subtypes_col, othertypes_col)),
+                      values = c(subtypes_col, othertypes_col)) +   
+  scale_shape_manual(name = "Cell type", labels = names(c(subtypes_col, othertypes_col)),
+                     values = c(rep(17, 5), rep(19, 7)))
+dev.off()
+
+
+#####################################
+# 2) load in MSN sub type labels plot
+## read in Logan BU snRNA dataset to label transfer
+obj_msn = here('data/tidy_data/Seurat_projects', 
+                    "BU_Run1_Striatum_subsetMSN_SCT_SeuratObj_N4.h5Seurat") %>% 
+  LoadH5Seurat()
+
+## plot MSN subtypes by subjects
+pdf(here(PLOTDIR, 'plots', 'BU_Run1_Striatum_UMAP_msn.ident.pdf'), width = 7.25, height = 4)
+DimPlot(object = obj_msn, reduction = "umap", group.by = 'orig.ident', 
+        split.by = 'orig.ident', cols = brewer.pal(n = 4, name = 'Set2') ) +
+  guides(color = guide_legend(nrow = 1, override.aes= list(size = 3))) + 
+  theme(legend.position = 'bottom')
+dev.off()
+
+## plot MSN subtypes by Seurat clusters
+pdf(here(PLOTDIR, 'plots', 'BU_Run1_Striatum_UMAP_msn.clusters.pdf'), width = 7.25, height = 4)
+DimPlot(object = obj_msn, reduction = "umap",  group.by = 'seurat_clusters', split.by = 'orig.ident') +
+  guides(color = guide_legend(nrow = 1, override.aes= list(size = 3))) + 
+  theme(legend.position = 'bottom')
+dev.off()
+
+## plot MSN subtypes by Macaque label transfer
+pdf(here(PLOTDIR, 'plots', 'BU_Run1_Striatum_UMAP_msn.macaqueLabels.pdf'), width = 7.25, height = 4)
+DimPlot(object = obj_msn, reduction = "umap", split.by = 'orig.ident', 
+        group.by = 'celltype2',  cols = subtypes_col) +
+  guides(color = guide_legend(nrow = 1, override.aes= list(size = 2))) + 
+  theme(legend.position = 'bottom')
+dev.off()
+
+pdf(here(PLOTDIR, 'plots', 'BU_Run1_Striatum_UMAP_msn.macaqueLabels2.pdf'), width = 7.25, height = 4)
+DimPlot(object = obj_msn, reduction = "umap", group.by = 'celltype2', 
+        shape.by = 'celltype2', pt.size = 1) +
+  scale_colour_manual(name = "Cell type", labels = names(c(subtypes_col)),
+                      values = c(subtypes_col)) +   
+  scale_shape_manual(name = "Cell type", labels = names(c(subtypes_col)),
+                     values = c(rep(17, 5)))
+dev.off()
+
+
+
+
