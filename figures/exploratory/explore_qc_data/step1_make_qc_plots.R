159130352480aa4cc34946577f59da63042aa4ca
badoi <badoi@me.com>
2022-05-17 10:06:09 -0400

    mapping statistics w/ successive sequencing

--- figures/exploratory/explore_qc_data/step1_make_qc_plots.R
+++ figures/exploratory/explore_qc_data/step1_make_qc_plots.R
@@ -6,6 +6,7 @@ options(stringsAsFactors = F)
 library(tidyverse)
 library(here)
 library(rcartocolor)
+# devtools::install_github("HuntsmanCancerInstitute/hciR")
 
 DATADIR='data/raw_data'
 PROJDIR='figures/exploratory/explore_qc_data'
@@ -14,40 +15,43 @@ PROJDIR='figures/exploratory/explore_qc_data'
 here(PROJDIR, c('plots', 'tables', 'rdas')) %>% sapply(dir.create, showWarnings = F)
 
 ## read in the phenotype data
-pd = read_csv(here(DATADIR, 'tables', 'OUD1_snRNA_seq_sampleSheet.csv')) %>%
+pd = read_csv(here('data/tidy_data', 'tables', 'LR_RM_BUMSR_Library_Index_Key_Run1_Run2.csv')) %>%
   rename_with(make.names) %>%
-  mutate(Center = ifelse(Region =='DLPFC', 'Singulomics', 'Boston University'), 
-         match1 = Sample.ID, match2 = paste(Sample.ID, Region, sep = '.'),
-         Sample.ID = gsub('HU', 'D-', Sample.ID), 
-         Subject = paste0('HU', ss(Sample.ID, '-', 2)))
+  mutate(Region = case_when(grepl('P',Sample) ~ 'Putamen',
+                            grepl('C',Sample) ~ 'Caudate'),
+         Subject = paste0('HU', ss(Sample, '-', 2))) %>%
+  dplyr::select(-c(Lane, Subject))
+
 
 #############################################
 ## read in the STARsolo mapping QC data tables
 map_qc_df = hciR::read_STAR(here(DATADIR, 'STARsolo_out')) %>%
-  dplyr::rename( 'match1' =  'sample') %>%
+  dplyr::rename( 'Sample' =  'sample') %>%
   mutate(stat = factor(stat, unique(stat)))  
+table(map_qc_df$Sample[!map_qc_df$Sample %in% pd$Sample])
 
-map_qc_df2 = inner_join(x = pd %>% dplyr::select(c(Sample.ID, match1, match2, Region, Pair, Case, Center)),
-                        y = map_qc_df, by = 'match1') %>%
-  dplyr::select(-c(match2, match1))
+map_qc_df2 = inner_join(x = pd %>% dplyr::select(c(Sample, Region)),
+                        y = map_qc_df) 
 
-plot_fn = here(PROJDIR, 'plots', 'Logan-OUD-snRNAseq.mapping_QC_stats.BUrun1_Singulomics.pdf')
+plot_fn = here(PROJDIR, 'plots', 'Logan-OUD-snRNAseq.mapping_QC_stats.BU_Run0-7.pdf')
 pdf(plot_fn, width = 6, height =4)
 for(x in levels(map_qc_df$stat)){
   tmp = map_qc_df2 %>%filter(stat %in% x)
   if(! all(tmp$value ==0)){
-  pp= ggplot(data = tmp, aes(x = Sample.ID, y = value, fill = Region)) +
+  pp= ggplot(data = tmp, aes(x = Sample, y = value, fill = Region)) +
     geom_bar(stat = 'identity') +
     scale_fill_carto_d(name = 'Tissue', palette = 'Safe') + ylab(x) + 
-    facet_grid(~Center + Region, space = 'free', scales = 'free_x') + 
+    facet_grid(~ Region, space = 'free', scales = 'free_x') + 
     theme_bw(base_size = 10) + 
-    theme(legend.position = 'bottom')
+    theme(legend.position = 'bottom') + 
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
   print(pp)
   }
 }
 dev.off()
 
-tab_fn = here(PROJDIR, 'tables', 'Logan-OUD-snRNAseq.mapping_QC_stats.BUrun1_Singulomics.xlsx')
+
+tab_fn = here(PROJDIR, 'tables', 'Logan-OUD-snRNAseq.mapping_QC_stats.BU_Run0-7.xlsx')
 map_qc_df2 %>%pivot_wider(values_from = 'value', names_from = 'stat') %>%
   writexl::write_xlsx(tab_fn)
 
@@ -59,34 +63,36 @@ map_qc_df2 %>%pivot_wider(values_from = 'value', names_from = 'stat') %>%
 ################################################################
 ######## read in STAR solo Count QC
 summary_qc_fn = list.files(path = here(DATADIR, 'STARsolo_out'), recursive = T,
-                       pattern = 'Summary.csv', full.names = T)
+                       pattern = 'Summary.csv', full.names = T) %>%
+  stringr::str_subset(pattern = 'GeneFull')
 names(summary_qc_fn) = ss(summary_qc_fn, '/', 9) %>% ss('\\.Solo',1)
 sumary_qc_df = summary_qc_fn %>% 
   lapply(read_csv, col_names = c('stat', 'value'),show_col_types = FALSE) %>%
-  data.table::rbindlist(idcol = 'match2') %>%
+  data.table::rbindlist(idcol = 'Sample') %>%
   mutate(stat = factor(stat, unique(stat)))
 
-sumary_qc_df2 = inner_join(x = pd%>% dplyr::select(c(Sample.ID, match1, match2, Region, Pair, Case, Center)),
-                           y = sumary_qc_df, by = 'match2') %>%
-  dplyr::select(-c(match2, match1))
+sumary_qc_df2 = inner_join(x = pd%>% dplyr::select(c(Sample, Region)),
+                           y = sumary_qc_df, by = 'Sample') 
 
-plot_fn2 = here(PROJDIR, 'plots', 'Logan-OUD-snRNAseq.summary_QC_stats.BUrun1_Singulomics.pdf')
+
+plot_fn2 = here(PROJDIR, 'plots', 'Logan-OUD-snRNAseq.summary_QC_stats.BU_Run0-7.pdf')
 pdf(plot_fn2, width = 6, height =4)
 for(x in levels(sumary_qc_df$stat)){
   tmp = sumary_qc_df2 %>%filter(stat %in% x)
   if(! all(tmp$value ==0)){
-    pp= ggplot(data = tmp, aes(x = Sample.ID, y = value, fill = Region)) +
+    pp= ggplot(data = tmp, aes(x = Sample, y = value, fill = Region)) +
       geom_bar(stat = 'identity') +
       scale_fill_carto_d(name = 'Tissue', palette = 'Safe') + ylab(x) + 
-      facet_grid(~Center + Region, space = 'free', scales = 'free_x') + 
+      facet_grid(~ Region, space = 'free', scales = 'free_x') + 
       theme_bw(base_size = 10) + 
-      theme(legend.position = 'bottom')
+      theme(legend.position = 'bottom') + 
+      theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
     print(pp)
   }
 }
 dev.off()
 
-tab_fn2 = here(PROJDIR, 'tables', 'Logan-OUD-snRNAseq.summary_QC_stats.BUrun1_Singulomics.xlsx')
+tab_fn2 = here(PROJDIR, 'tables', 'Logan-OUD-snRNAseq.summary_QC_stats.BU_Run0-7.xlsx')
 sumary_qc_df2 %>%pivot_wider(values_from = 'value', names_from = 'stat') %>%
   writexl::write_xlsx(tab_fn2)
 
