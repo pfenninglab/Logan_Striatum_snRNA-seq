31782d3369c04ccf3f2c1aa1eacfcb72aba8a65d
badoi <badoi@me.com>
2022-10-21 21:21:59 -0400

    update file

--- code/final_code/download_data/step1_sync_lane_scc.sh
+++ code/final_code/download_data/step1_sync_lane_scc.sh
@@ -7,15 +7,13 @@ BU_DIR=/restricted/projectnb/singlecell-rl/Logan_BU_Striatum_snRNA-seq
 
 mkdir -p $BU_DIR/data/raw_data
 
-rsync -Pa $LANEDIR/code $BU_DIR
+rsync -Pavh --delete $LANEDIR/code $BU_DIR
 
-rsync -Pa $LANEDIR/figures $BU_DIR
+rsync -Pavh --delete $LANEDIR/figures $BU_DIR
 
-rsync -Pa $LANEDIR/data/tidy_data $BU_DIR/data
+rsync -Pavh --delete $LANEDIR/data/tidy_data $BU_DIR/data
 
 ###########################################
 ######## transfer from SCC to Lane ########
-rsync -Pan $BU_DIR/* $LANEDIR
-
-rsync -Pa $BU_DIR/* $LANEDIR
+rsync -Pavh $BU_DIR/ $LANEDIR
 
