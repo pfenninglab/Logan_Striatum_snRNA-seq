c38776ea7191cdca087a026d5f73fa10d0924939
badoi <badoi@me.com>
2022-11-11 08:55:24 -0500

    voom limma w/ means

--- code/raw_code/differential_expression_analysis/step5_pseudoBulk_voomLimma_diffGene_bigModelSVA.R
+++ code/raw_code/differential_expression_analysis/step5_pseudoBulk_voomLimma_diffGene_bigModelSVA.R
@@ -23,9 +23,6 @@ library(swfdr)
 ## regress out the surrogate variables
 library(jaffelab)
 
-## to make the output data tidy
-library(biobroom)
-
 ss <- function(x, pattern, slot = 1, ...) { 
   sapply(strsplit(x = x, split = pattern, ...), '[', slot) }
 options(stringsAsFactors = F)
@@ -39,56 +36,104 @@ DATADIR='data/tidy_data/differential_expression_analysis'
 plan("multicore", workers = 28)
 options(future.globals.maxSize = 80 * 1024^3)
 
+
 ##################################################
 # 1) create or load pseudobulk sce object
-save_pseudobulk =here(DATADIR, 'rdas', 'OUD_Striatum_refined_all_PseudoBulk_N22.sce2.rds')
-pb = readRDS(save_pseudobulk)
-
+save_pseudobulk =here(DATADIR, 'rdas', 'BU_OUD_Striatum_refined_all_PseudoBulk_N22.sce2.rds')
+if(!file.exists(save_pseudobulk)){
+  ## load the single cell counts
+  h5Dir =here(DATADIR, 'HDF5Array'); dir.create(h5Dir, showWarnings = F)
+  sce = loadHDF5SummarizedExperiment(h5Dir, prefix="BU_OUD_Striatum_refined_all_SeuratObj_N22.h5Seurat")
+  
+  ## merge interneurons again
+  sce$celltype3 = ifelse(grepl('Int', sce$celltype3), 'Interneuron',sce$celltype3)
+  table(sce$celltype3)
+  
+  ## aggregate by cluster-sample to create pseudo-bulk count matrix
+  colData(sce)
+  groups <- colData(sce)[, c("celltype3", "Case", 'Region')]
+  pb <- aggregate.Matrix(t(counts(sce)), groupings = groups, fun = "sum") 
+  dim(pb)
+  
+  ## split by cluster, transform & rename columns
+  pb_colData = colData(sce) %>% as.data.frame() %>%
+    rownames_to_column('match') %>% 
+    mutate(Pair = factor(Pair), match = paste(celltype3, Case, Region, sep = '_')) %>% 
+    filter(!duplicated(match)) %>% column_to_rownames('match')
+  pb_colData = pb_colData[rownames(pb),]
+  
+  ## make sure this PD is correct
+  with(pb_colData, table(Case, celltype3, Region))
+  
+  ## add number of cells per aggregate
+  num_cells = groups %>% as.data.frame() %>% 
+    mutate(tmp = paste(celltype3, Case, Region,sep= '_')) %>% 
+    pull(tmp) %>% table()
+  num_cells %>% as.numeric() %>% summary()
+  pb_colData$numCells = num_cells[rownames(pb_colData)]
+  
+  ## add the gene detection rate
+  pb_colData$cdr <- scale(rowMeans(pb > 0)) 
+  
+  ## create SingleCellExperiment from pseudo bulk counts across all cell types and region
+  (pb <- SingleCellExperiment(assays = t(pb), colData = pb_colData))
+  
+  ## remap case index nested inside OUD dx
+  remap_case_idx = split(pb$Case, pb$DSM.IV.OUD) %>% 
+    lapply(function(x){
+      x = setNames(LETTERS[as.numeric(factor(x))], x)
+      x[!duplicated(x)]
+    }) %>% unlist()
+  names(remap_case_idx) = ss(names(remap_case_idx), '\\.', 2)
+  pb$CaseIdx = remap_case_idx[as.character(pb$Case)]
+  
+  ## check this is correct
+  table(pb$DSM.IV.OUD, pb$CaseIdx)
+  table(pb$celltype3, pb$CaseIdx)
+  table(pb$celltype3, pb$Case)
+  
+  saveRDS(pb, save_pseudobulk)
+} else {
+  pb = readRDS(save_pseudobulk)
+}
 
 ####################################################
 ## 2) filter pseudobulk samples that have too few cells
 pb = pb[, pb$numCells > 20]
+pb = pb[, pb$celltype3 != 'Mural'] # drop mural cells b/c too few
 pb$celltype3 = make.names(pb$celltype3) %>% as.factor()
+pb$celltype_dx = interaction(pb$DSM.IV.OUD, pb$celltype3) %>% as.factor()
+table(pb$celltype_dx)
+
 pb$Region = as.factor(pb$Region)
 pb$Sex = as.factor(pb$Sex)
 
-
 ## construct design & contrast matrix regressing out the DetRate
-design <- model.matrix(~ cdr + Age + Sex + PMI + RIN + Region + # co-variates 
-                         celltype3 + # nested variability of cell type in subject
-                         DSM.IV.OUD:celltype3 + # celltype-specific effect in OUD
-                         DSM.IV.OUD:Sex + # sex-specific effect in OUD
-                         DSM.IV.OUD:Region + # region-specific effect in OUD
-                         DSM.IV.OUD, # main effect
+design <- model.matrix(~ 0 + cdr + Age + Sex + PMI + RIN + Region + # co-variates 
+                         celltype_dx + # nested variability of cell type and Dx
+                         celltype_dx:Sex + # sex-specific effect in OUD and celltype
+                         celltype_dx:Region, # region-specific effect in OUD and celltype
                        data = colData(pb))
 
-all.zero <- apply(design, 2, function(x) all(x==0))
-all.zero[all.zero]
-idx <- which(colnames(design) %in% c('celltype3Mural:DSM.IV.OUDOUD') | 
-               all.zero)
-design <- design[,-idx]
-
-
 ## construct the null model, used in regressing out the various factors
-design0 <- model.matrix(~ cdr + Age + Sex + PMI + RIN + Region + celltype3, # co-variates 
+design0 <- model.matrix(~ 0 + cdr + Age + Sex + PMI + RIN + Region + celltype3, # co-variates 
                         data = colData(pb))
-
 
 ####################################
 # 3) normalization using voom-limma
 y <- DGEList(counts = assays(pb)[[1]])
-dim(y)
+dim(y) # 31611   203
 
 ## filter out genes w/ low counts
 A <- rowMeans(y$counts)
 isexpr <- A > 5
 y <- y[isexpr, , keep.lib.size = FALSE]
-dim(y)
+dim(y) # 20409   203
 
 ## filter out ribosomal genes, filter out mitochondria genes
 keep.genes <- grep("^RP[SL]|^MT-",rownames(y), value = T, invert = T)
 y = y[keep.genes, , keep.lib.size = FALSE]
-dim(y) #  20273   205
+dim(y) #  20299   203
 
 # normalize counts
 y <- calcNormFactors(y)
@@ -96,26 +141,26 @@ y <- calcNormFactors(y)
 ## voom precision weights and sample-wise quality weights normalization
 v <- voomWithQualityWeights(y, design)
 cor <- duplicateCorrelation(v, design, block = colData(pb)$Case)
-cor$consensus # 0.2732756
+cor$consensus # 0.2617345
 
 ## recalculate weights after adjusting for correlated samples from same subject
 v <- voomWithQualityWeights(y, design, block = colData(pb)$Case, 
           correlation = cor$consensus)
 cor <- duplicateCorrelation(v, design, block = colData(pb)$Case)
-cor$consensus # 0.2719249
+cor$consensus # 0.2339142
 
 
 ############################################################
 # 4) Use surrogate variables to estimate unmodeled variation
 
 ## estimate the number of SVs from the adjusted
-save_sva =here(DATADIR, 'rdas', 'OUD_Striatum_refined_all_PseudoBulk_N22.sva.rds')
+save_sva =here(DATADIR, 'rdas', 'BU_OUD_Striatum_refined_all_PseudoBulk_N22.sva.rds')
 if(! file.exists(save_sva)){
-  (n.sv = num.sv(v$E, design, method="be", seed = set.seed(1))) #30
+  (n.sv = num.sv(v$E, design, method="be", seed = set.seed(1))) #23
   svobj = sva(v$E, design, design0, n.sv=n.sv, B = 20)
   saveRDS(svobj, save_sva)
 } else {
-  svobj = readRDS(svobj)
+  svobj = readRDS(save_sva)
 }
 
 ## add the SVs to the model matrix
@@ -126,121 +171,119 @@ design0SV = cbind(design0, svobj$sv)
 v <- voomWithQualityWeights(y, designSV, block = colData(pb)$Case, 
                             correlation = cor$consensus)
 cor <- duplicateCorrelation(v, designSV, block = colData(pb)$Case)
-cor$consensus # 0.08238471
+cor$consensus # 0.08645557
 
-save_voom = here(DATADIR, 'rdas', 'voomLimma_norm_object_N205pb.rds')
+save_voom = here(DATADIR, 'rdas', 'voomLimma_norm_object_N222pb.rds')
 saveRDS(v, file = save_voom)
-
-## regress out the un-modeled variation in the data
-v_clean = v
-v_clean$E <- cleaningY(v$E, designSV, P = ncol(design))
-
-save_voom_clean = here(DATADIR, 'rdas', 'voomLimma_clean_object_N205pb.rds')
-saveRDS(v_clean, file = save_voom_clean)
 
 ####################################################################
 ## 6) fit the model to get DEGs for various differences in the data
 fit <- lmFit(v, designSV, block = colData(pb)$Case, correlation = cor$consensus)
-fit <- eBayes(fit)
-
-tests = decideTests(fit,method = "separate")
-summary(tests)
+fit <- eBayes(fit, robust = TRUE)
 
 save_fit = here(DATADIR, 'rdas', 'voomLimma_diffGene_bigModelFitSVA.rds')
 saveRDS(fit, file = save_fit)
 
-####################################################################
-## 7) get the DEG tables and peak at interesting DEGs and no. of DEGs
+save_design = here(DATADIR, 'rdas', 'bigModelFitSVA_designMatrix.rds')
+saveRDS(designSV, file = save_design)
 
-## get cell type DEGs
-celltype_effect = tests %>% as.data.frame() %>% names() %>% 
-  grep('celltype3', .,value = T) %>% grep('DSM.IV.OUDOUD', ., value = T) 
-names(celltype_effect) = gsub('celltype3|:DSM.IV.OUDOUD', '', celltype_effect)
-deg_celltypes = lapply(celltype_effect, topTable, fit = fit, n=Inf)
+###########################################################
+## 7) compute the differences b/t Dx within each cell type
+celltypes=levels(factor(pb$celltype3 %>% make.names()))
+designSV2 =designSV
+colnames(designSV2) = make.names(colnames(designSV2))
 
-## other effects
-main_effect = tests %>% as.data.frame() %>% names() %>% 
-  grep('celltype3', .,value = T, invert = T) %>% grep('DSM.IV.OUDOUD', ., value = T) 
-names(main_effect) = gsub('OUD$|:DSM.IV.OUDOUD', '', main_effect)
-deg_main = lapply(main_effect, topTable, fit = fit, n=Inf)
-
-## use SWFDR to increase power of detecting DEGs based on avg expression covariate
-## https://pubmed.ncbi.nlm.nih.gov/30581661/
-deg_list = c(deg_main, deg_celltypes) %>% lapply(function(x){
-  x %>% dplyr::rename('p_adj' = 'adj.P.Val') %>% rownames_to_column('gene')
+## make the cell type contrats
+con_celltypes = sapply(setNames(celltypes, celltypes),function(cell) {
+  OUD = paste0('celltype_dxOUD.',cell) %>% make.names()
+  CTL = paste0('celltype_dxCTL.',cell) %>% make.names()
+  
+  OUDSexM = paste0('SexM.celltype_dxOUD.',cell) %>% make.names()
+  CTLSexM = paste0('SexM.celltype_dxCTL.',cell) %>% make.names()
+  
+  OUDRegP = paste0('RegionPutamen.celltype_dxOUD.',cell) %>% make.names()
+  CTLRegP = paste0('RegionPutamen.celltype_dxCTL.',cell) %>% make.names()
+  
+  paste('(',OUD,'+',OUDSexM,'+',OUDRegP,')/3',
+        '-(',CTL,'+',CTLSexM,'+', CTLRegP,')/3')
 })
 
-deg_list = deg_list %>% 
-  lapply(function(x){
-    x %>% mutate(p_adj =  lm_qvalue(P.Value, X=AveExpr)$q)
-  })
+## the first cell type x dx is the reference cell type
+con_celltypes[1] = "(celltype_dxOUD.Astrocytes + SexM.celltype_dxOUD.Astrocytes + RegionPutamen.celltype_dxOUD.Astrocytes)/3" 
+
+## create the contrasts for OUD effect Between all cells or major classes
+con_groups = c('All' = paste('(', paste(con_celltypes, collapse = ' + '), 
+                             ')/', length(con_celltypes)), 
+               'Neuron' = paste('(', paste(con_celltypes[grepl('^D|^Int',celltypes)], collapse = ' + '), 
+                                ')/', sum(grepl('^D|^Int',celltypes))), 
+               'Glia' =  paste('(', paste(con_celltypes[!grepl('^D|^Int',celltypes)], collapse = ' + '),
+                               ')/', sum(!grepl('^D|^Int',celltypes))))
+
+## refit the model based on these contrasts
+cont.matrix <- makeContrasts(contrasts= c(con_groups, con_celltypes), levels=designSV2)
+rownames(cont.matrix) = colnames(designSV)
+fit2 <- contrasts.fit(fit, cont.matrix) %>% eBayes()
+
+## compute the DEGs from these contrasts
+deg_list = lapply(setNames(colnames(cont.matrix),  names(c(con_groups, con_celltypes))), 
+                  function(coef){
+  topTable(coef = coef, fit =fit2, n=Inf) %>% arrange(P.Value) %>% 
+  ## use SWFDR to increase power of detecting DEGs based on avg expression covariate
+  ## https://pubmed.ncbi.nlm.nih.gov/30581661/
+  mutate(adj.P.Val.Within =  lm_qvalue(P.Value, X=AveExpr)$q) %>%
+  dplyr::select(-adj.P.Val) %>% rownames_to_column('gene') 
+})
+
+## FDR correction Between all tests
+deg_list = deg_list %>% data.table::rbindlist(idcol = 'celltype') %>% 
+  mutate(adj.P.Val.Between =  lm_qvalue(P.Value, X=AveExpr)$q) %>%
+  split(by = 'celltype')
+
+# FDR cutoff
+sapply(deg_list, function(x) x[x$adj.P.Val.Within < 0.05,] %>% nrow())
+sapply(deg_list, function(x) x[x$adj.P.Val.Between < 0.05,] %>% nrow())
+
+# lower confidence cutoff
+sapply(deg_list, function(x) x[x$P.Value < 0.01,] %>% nrow())
 
 ####################################################################
 ## 8) save the output of voom_limma differential state analyses
 rdasDir =file.path(DATADIR, 'rdas'); dir.create(rdasDir, showWarnings = F)
-save_res_fn = here(rdasDir, 'OUD_Striatum_voom_limma_bigModelSVA_N22.rds')
+save_res_fn = here(rdasDir, 'OUD_Striatum_voom_limma_bigModelSVA_N22.celltype.rds')
 saveRDS(deg_list, save_res_fn)
 
 tablesDir =file.path(DATADIR, 'tables'); dir.create(tablesDir, showWarnings = F)
-save_res_fn2 = here(tablesDir, 'OUD_Striatum_voom_limma_bigModelSVA_N22.xlsx')
+save_res_fn2 = here(tablesDir, 'OUD_Striatum_voom_limma_bigModelSVA_N22.celltype.xlsx')
 deg_list %>% lapply(function(x) x %>% arrange(P.Value)) %>% writexl::write_xlsx(save_res_fn2)
 
-alpha = 0.01
 # save tables DEGs w/ P.Value < alpha up and down regulated
-save_res_fn3 = here(tablesDir, 'OUD_Striatum_voom_limma_bigModelSVA_N22.lowConfCutOff.upReg.xlsx')
+save_res_fn3 = here(tablesDir, 'OUD_Striatum_voom_limma_bigModelSVA_N22.celltype.lowConfCutOff.upReg.xlsx')
 deg_list %>% lapply(function(x) x %>% arrange(P.Value) %>% 
-                      filter(P.Value < alpha, logFC > 0)) %>% 
+                      filter(P.Value < 0.01, logFC > 0)) %>% 
   writexl::write_xlsx(save_res_fn3)
 
-save_res_fn4 = here(tablesDir, 'OUD_Striatum_voom_limma_bigModelSVA_N22.lowConfCutOff.dnReg.xlsx')
+save_res_fn4 = here(tablesDir, 'OUD_Striatum_voom_limma_bigModelSVA_N22.celltype.lowConfCutOff.dnReg.xlsx')
 deg_list %>% lapply(function(x) x %>% arrange(P.Value) %>% 
-                      filter(P.Value < alpha, logFC < 0)) %>% 
+                      filter(P.Value < 0.01, logFC < 0)) %>% 
   writexl::write_xlsx(save_res_fn4)
 
 
 #######################################
 ## 9) check out some interesting genes
-
-# FDR cutoff
-sapply(deg_list, function(x) x[x$p_adj < 0.05,] %>% nrow())
-
-# lower confidence cutoff
-sapply(deg_list, function(x) x[x$P.Value < 0.01,] %>% nrow())
-
-
 sapply(deg_list, function(x){
   x %>% filter(gene %in% c('CRHR1', 'LRP8'))%>% 
-    pull(p_adj)
+    pull(adj.P.Val.Between)
 })
 
 lapply(deg_list, function(x){
-  x %>% filter(p_adj < alpha)%>% arrange(p_adj) %>% pull(gene) %>% 
-    paste(collapse = ', ')
-})
-
-lapply(deg_list, function(x){
-  x %>% filter(p_adj < alpha) %>% pull(gene) %>% 
-    str_subset('^OPR|PDYN|PENK|POMC|^DRD') %>% 
+  x %>% filter(adj.P.Val.Between < 0.05) %>% pull(gene) %>% 
     paste(collapse = ', ')
 })
 
 
 lapply(deg_list, function(x){
-  x %>% filter(P.Value < 0.01) %>% pull(gene) %>% 
-    str_subset('^OPR|PDYN|PENK|POMC|^DRD') %>% 
-    paste(collapse = ', ')
-})
-
-
-lapply(deg_list, function(x){
-  x %>% filter(p_adj < alpha) %>% pull(gene) %>% 
+  x %>% filter(adj.P.Val.Between < 0.05) %>% pull(gene) %>% 
     str_subset('^FOX|^GAD|^CHAT$|^TH$|^KCN|^SCN|^SLC') %>% 
     paste(collapse = ', ')
 })
 
-
-lapply(deg_list, function(x){
-  x %>% filter(P.Value < 0.01) %>% pull(gene) %>% 
-    str_subset('^FOX|^GAD|^CHAT$|^TH$|^KCN|^SCN|^SLC') %>% 
-    paste(collapse = ', ')
-})
---
+++ code/raw_code/differential_expression_analysis/step7_pseudoBulk_voomLimma_diffGene_bigModelSVA.interactionRegion.R
@@ -0,0 +1,107 @@
+## conda activate r4
+## packages for data table processing 
+library(here)
+library(tidyverse)
+library(rlang)
+library(writexl)
+
+## main differential gene expression package
+library(SingleCellExperiment)
+library(limma)
+library(edgeR)
+library(swfdr)
+
+ss <- function(x, pattern, slot = 1, ...) { 
+  sapply(strsplit(x = x, split = pattern, ...), '[', slot) }
+options(stringsAsFactors = F)
+options(repr.plot.width=11, repr.plot.height=8.5)
+
+DATADIR='data/tidy_data/differential_expression_analysis'
+
+###################################################
+## 1) read in the fitted big model w/ SVA features 
+save_pseudobulk =here(DATADIR, 'rdas', 'BU_OUD_Striatum_refined_all_PseudoBulk_N22.sce2.rds')
+pb = readRDS(save_pseudobulk)
+pb = pb[, pb$numCells > 20]
+pb = pb[, pb$celltype3 != 'Mural'] # drop mural cells b/c too few
+
+save_fit = here(DATADIR, 'rdas', 'voomLimma_diffGene_bigModelFitSVA.rds')
+fit = readRDS(file = save_fit)
+
+save_design = here(DATADIR, 'rdas', 'bigModelFitSVA_designMatrix.rds')
+designSV = readRDS(file = save_design)
+
+###########################################################
+## 2) compute the differences b/t Dx within each cell type
+celltypes=levels(factor(pb$celltype3 %>% make.names()))
+designSV2 =designSV
+colnames(designSV2) = make.names(colnames(designSV2))
+
+## make the cell type contrasts
+con_celltypes = sapply(setNames(celltypes, celltypes),function(cell) {
+  OUD = paste0('celltype_dxOUD.',cell) %>% make.names()
+  CTL = paste0('celltype_dxCTL.',cell) %>% make.names()
+  
+  OUDRegionPutamen = paste0('RegionPutamen.celltype_dxOUD.',cell) %>% make.names()
+  CTLRegionPutamen = paste0('RegionPutamen.celltype_dxCTL.',cell) %>% make.names()
+
+  paste('(',OUDRegionPutamen, '-',OUD, ')-(',CTLRegionPutamen, '-',CTL,')')
+})
+
+## the first cell type x dx is the reference cell type
+con_celltypes[1] = "( RegionPutamen.celltype_dxOUD.Astrocytes - celltype_dxOUD.Astrocytes )" 
+
+## create the contrasts for OUD effect Between all cells or major classes
+con_groups = c('All' = paste('(', paste(con_celltypes, collapse = ' + '), 
+                             ')/', length(con_celltypes)), 
+             'Neuron' = paste('(', paste(con_celltypes[grepl('^D|^Int',celltypes)], collapse = ' + '), 
+                              ')/', sum(grepl('^D|^Int',celltypes))), 
+             'Glia' =  paste('(', paste(con_celltypes[!grepl('^D|^Int',celltypes)], collapse = ' + '),
+                             ')/', sum(!grepl('^D|^Int',celltypes))))
+
+## refit the model based on these contrasts
+cont.matrix <- makeContrasts(contrasts= c(con_groups, con_celltypes), levels=designSV2)
+rownames(cont.matrix) = colnames(designSV)
+fit2 <- contrasts.fit(fit, cont.matrix) %>% eBayes()
+
+## compute the DEGs from these contrasts
+deg_list = lapply(setNames(colnames(cont.matrix),  names(c(con_groups, con_celltypes))), 
+                  function(coef){
+                    topTable(coef = coef, fit =fit2, n=Inf) %>% arrange(P.Value) %>% 
+                      ## use SWFDR to increase power of detecting DEGs based on avg expression covariate
+                      ## https://pubmed.ncbi.nlm.nih.gov/30581661/
+                      mutate(adj.P.Val.Within =  lm_qvalue(P.Value, X=AveExpr)$q) %>%
+                      dplyr::select(-adj.P.Val) %>% rownames_to_column('gene') 
+                  })
+
+## FDR correction Between all tests
+deg_list = deg_list %>% data.table::rbindlist(idcol = 'celltype') %>% 
+  mutate(adj.P.Val.Between =  lm_qvalue(P.Value, X=AveExpr)$q) %>%
+  split(by = 'celltype')
+
+# FDR cutoff
+sapply(deg_list, function(x) x[x$adj.P.Val.Within < 0.05,] %>% nrow())
+sapply(deg_list, function(x) x[x$adj.P.Val.Between < 0.05,] %>% nrow())
+
+# lower confidence cutoff
+sapply(deg_list, function(x) x[x$P.Value < 0.01,] %>% nrow())
+
+####################################################################
+## 8) save the output of voom_limma differential state analyses
+rdasDir =file.path(DATADIR, 'rdas'); dir.create(rdasDir, showWarnings = F)
+save_res_fn = here(rdasDir, 'OUD_Striatum_voom_limma_bigModelSVA_N22.regionInteraction.rds')
+saveRDS(deg_list, save_res_fn)
+
+tablesDir =file.path(DATADIR, 'tables'); dir.create(tablesDir, showWarnings = F)
+save_res_fn2 = here(tablesDir, 'OUD_Striatum_voom_limma_bigModelSVA_N22.regionInteraction.xlsx')
+deg_list %>% lapply(function(x) x %>% arrange(P.Value)) %>% writexl::write_xlsx(save_res_fn2)
+
+# save tables DEGs w/ P.Value < alpha up and down regulated
+save_res_fn3 = here(tablesDir, 'OUD_Striatum_voom_limma_bigModelSVA_N22.regionInteraction.lowConfCutOff.upReg.xlsx')
+deg_list %>% lapply(function(x) x %>% arrange(P.Value) %>% filter(P.Value < 0.01, logFC > 0)) %>% 
+  writexl::write_xlsx(save_res_fn3)
+
+save_res_fn4 = here(tablesDir, 'OUD_Striatum_voom_limma_bigModelSVA_N22.regionInteraction.lowConfCutOff.dnReg.xlsx')
+deg_list %>% lapply(function(x) x %>% arrange(P.Value) %>% filter(P.Value < 0.01, logFC < 0)) %>% 
+  writexl::write_xlsx(save_res_fn4)
+
