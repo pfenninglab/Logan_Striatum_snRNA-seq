f3a853c8db1e7cf3e6846a0c92920b498874b86b
badoi <badoi@me.com>
2022-12-29 11:11:47 -0500

    add plots for glia DEGs

---
+++ figures/explanatory/figure4_the_glia_degs_and_cell_states/figure4_pathway_clustering_glia.R
@@ -0,0 +1,125 @@
+## packages for data table processing 
+library(tidyverse)
+library(igraph)
+library(network)
+library(sna)
+library(stringr)
+library(RColorBrewer)
+library(data.table)
+library(here)
+library(leiden)
+
+ss <- function(x, pattern, slot = 1, ...) { 
+  sapply(strsplit(x = x, split = pattern, ...), '[', slot) }
+options(stringsAsFactors = F)
+options(repr.plot.width=11, repr.plot.height=8.5)
+source(here('code/final_code/Rutils/igraph_pathway_clustering.R'))
+
+FIGDIR='figures/explanatory/figure4_the_glia_degs_and_cell_states'
+dir.create(here(FIGDIR, 'plots', 'gsea'), recursive = T, showWarnings = F)
+dir.create(here(FIGDIR, 'tables'), recursive = T, showWarnings = F)
+
+###################################
+# 0) pre-set colors and cell types 
+celltypes = c('D1-Matrix', 'D2-Matrix',  'D1-Striosome', 'D2-Striosome',
+              'D1/D2-Hybrid', 'Interneuron',  'Astrocytes', 'Endothelial', 
+              'Microglia', 'Mural', 'Oligos', 'Oligos_Pre')
+
+neuron_types = c('D1-Matrix', 'D2-Matrix',  'D1-Striosome', 'D2-Striosome',
+                 'D1/D2-Hybrid', 'Interneuron', 'Neuron') %>% make.names()
+
+glia_types = c('Astrocytes', 'Endothelial', 'Microglia', 'Mural', 'Oligos', 
+               'Oligos_Pre', 'Glia', 'All') %>% make.names()
+
+in2mm<-25.4
+
+###########################################
+# 1) read in the pathway enrichment tables
+xl_path = here('data/tidy_data/differential_expression_analysis', 
+               'tables/OUD_Striatum_GSEA_enrichment_msigdb_H_C2_C5_SynGO.xlsx')
+readxl::excel_sheets(path = xl_path)
+
+## read in the comparison of OUD vs. CTL averaged across all subtypes
+gsea_list <- readxl::read_excel(path = xl_path, sheet = 'All')
+gsea_df = gsea_list%>% filter(celltype %in% glia_types)
+  
+## subset by positive and negative NES to create two pathway networks
+with(gsea_df, table(celltype, NES > 0 ))
+net_pos=make_igraph_from_pathways(gsea_df %>% filter(NES > 0))
+net_neg=make_igraph_from_pathways(gsea_df %>% filter(NES < 0))
+
+#########################################################
+# 2) craft the networks to create the pathway clustering
+
+## add the colors to the POSITIVE NES
+pos_range = max(V(net_pos)$NES) - min(V(net_pos)$NES)
+col_pos = circlize::colorRamp2(seq(min(V(net_pos)$NES), max(V(net_pos)$NES), pos_range/8), 
+                               brewer.pal(9,"BuGn"))
+V(net_pos)$color= col_pos(V(net_pos)$NES)
+
+## add the colors to the NEGATIVE NES
+neg_range = max(V(net_neg)$NES) - min(V(net_neg)$NES)
+col_neg = circlize::colorRamp2(seq(min(V(net_neg)$NES), max(V(net_neg)$NES), neg_range/8), 
+                               rev(brewer.pal(9,"PuRd")))
+V(net_neg)$color= col_neg(V(net_neg)$NES)
+
+##############################################################################
+## trim and cluster the POSITIVE network; CHANGE target_degree if you have too 
+## many edges, clustering will not work + you will get a hairball network
+net_pos.sp = trim_edges(net_pos, target_degree = .2)
+clp_pos <- cluster_leiden(net_pos.sp, resolution_parameter = 1.5, 
+                      objective_function = 'modularity', n_iterations = 10)
+pdf(here(FIGDIR, 'plots','gsea','figure4_clustered_glia_pos_gsea_pathway_network.pdf'))
+plot(clp_pos, net_pos.sp, vertex.label=NA)
+dev.off()
+
+net_pos_list = trim_clustered_nodes(net_pos.sp, clp_pos, min_nodes = 3)
+V(net_pos_list$net)$hub_score = hub_score(net_pos_list$net)$vector
+V(net_pos_list$net)$label.cex = .5
+
+##############################################################################
+## trim and cluster the NEGATIVE network; CHANGE target_degree if you have too 
+## many edges, clustering will not work + you will get a hairball network
+net_neg.sp = trim_edges(net_neg, target_degree = .15)
+clp_neg <- cluster_leiden(net_neg.sp, resolution_parameter = 1, 
+                      objective_function = 'modularity',  n_iterations = 10)
+pdf(here(FIGDIR, 'plots','gsea','figure4_clustered_glia_neg_gsea_pathway_network.pdf'))
+plot(clp_neg, net_neg.sp, vertex.label=NA)
+dev.off()
+
+net_neg_list = trim_clustered_nodes(net_neg.sp, clp_neg, min_nodes = 3)
+V(net_neg_list$net)$mem = V(net_neg_list$net)$mem + max(V(net_pos_list$net)$mem)
+V(net_neg_list$net)$hub_score = hub_score(net_neg_list$net)$vector
+V(net_neg_list$net)$label.cex = .5
+
+
+###################################################################
+## clean up the labels, merging the pathways from both clusterings
+vert.meta =  bind_rows(vertex_attr(net_pos_list$net), 
+                       vertex_attr(net_neg_list$net)) %>% as.data.frame()
+to_label = vert.meta %>% clean_pathways()
+to_label_num = vert.meta %>% dplyr::select(name, mem) %>% deframe()
+
+## make sets of 3 plots for the current POSITIVE network
+prefix_pos = here(FIGDIR, 'plots','gsea','figure4_clustered_glia_pos_gsea_pathway_network')
+plot_clusterings_3set(net_pos_list, prefix_pos, to_label, to_label_num, 
+                      height = 60/in2mm, width = 60/in2mm)
+plot_pt_legend(V(net_neg_list$net)$size, prefix_pos, height = 20/in2mm, width = 10/in2mm)
+plot_col_legend(prefix = prefix_pos, col = col_pos, fontsize = 5,
+                height = 8/in2mm, width = 30/in2mm)
+
+
+## make sets of 3 plots for the current NEGATIVE network
+prefix_neg = here(FIGDIR, 'plots','gsea','figure4_clustered_glia_neg_gsea_pathway_network')
+plot_clusterings_3set(net_neg_list, prefix_neg, to_label, to_label_num, 
+                      height = 60/in2mm, width = 60/in2mm)
+plot_col_legend(prefix = prefix_neg, col = col_neg, fontsize = 5,
+                height = 8/in2mm, width = 30/in2mm)
+
+
+## add the clustered pathways back into the 
+curenrich_clustered = gsea_df %>% 
+  mutate(cluster_number = to_label_num[ paste0(celltype, '#', pathway)]) %>% 
+  arrange(cluster_number, padj) %>%
+  relocate(cluster_number, pathway, description,.after = 'celltype') %>% 
+  writexl::write_xlsx(here(FIGDIR, 'tables','figure4_clustered_glia_gsea_pathway_network.xlsx'))
---
+++ figures/explanatory/figure4_the_glia_degs_and_cell_states/figure4_plot_celltype_DEGs_heatmap_glia.R
@@ -0,0 +1,146 @@
+ss <- function(x, pattern, slot = 1, ...) { 
+  sapply(strsplit(x = x, split = pattern, ...), '[', slot) }
+options(stringsAsFactors = F)
+
+## packages for data table processing/plotting
+library(SingleCellExperiment)
+library(ComplexHeatmap)
+library(rcartocolor)
+library(data.table)
+library(tidyverse)
+library(ggsci)
+library(here)
+library(readxl)
+library(ggh4x)
+library(wesanderson)
+library(cividis) #  devtools::install_github("marcosci/cividis")
+
+
+DATADIR='data/tidy_data/differential_expression_analysis'
+
+## make for this subdirs
+PLOTDIR='figures/explanatory/figure4_the_glia_degs_and_cell_states'
+here(PLOTDIR, c('plots', 'tables', 'rdas')) %>% sapply(dir.create, showWarnings = F)
+
+###################################
+# 0) pre-set plotting aesthetics
+subtypes = c('D1-Matrix', 'D2-Matrix',  'D1-Striosome', 'D2-Striosome','D1/D2-Hybrid')
+subtypes_col = c('#1f78b4', '#a6cee3', '#e31a1c', '#fb9a99',  '#6a3d9a')
+names(subtypes_col) = subtypes %>% make.names()
+
+othertypes = c('Int-CCK', 'Int-PTHLH','Int-SST', 'Int-TH', 
+               'All', 'Neuron', 'Glia',
+               'Astrocytes', 'Endothelial', 'Microglia', 'Mural', 'Oligos', 
+               'Oligos_Pre', 'Interneuron')
+othertypes_col = c(carto_pal(4, "Safe"), 
+                   mypal = pal_npg('nrc')(3),
+                   carto_pal(length(othertypes) -7 , "Vivid"))
+names(othertypes_col) = othertypes
+othertypes_col = othertypes_col[-c(1:4)]
+typecolors = c(othertypes_col[1:3], subtypes_col, othertypes_col[c(10, 4:9)])
+
+dx_col = setNames(pal_npg(palette = c("nrc"), alpha = 1)(2), c('CTL', 'OUD'))
+
+in2mm<-25.4
+my_theme = theme_classic(base_size = 6)
+
+###########################################
+# 1) load the pseudobulk, regressed matrix
+z_clean = readRDS(here(DATADIR, 'rdas', 'voomLimma_zscore_object_N222pb_regressedCovariate.rds'))
+
+## grab the phenotype data
+df = here(DATADIR, 'rdas', 'BU_OUD_Striatum_refined_all_PseudoBulk_N22.sce2.rds') %>% 
+  readRDS() %>% colData() %>% as.data.frame() %>% 
+  dplyr::select(ID:CaseIdx) %>% 
+  mutate(celltype3 = celltype3 %>% make.names %>% factor(names(typecolors)),
+         celltype3 = droplevels(celltype3), 
+         celltype_class = case_when(grepl('^D|^Int', celltype3)~ 'Neuron',  
+                                    TRUE ~ 'Glia'), 
+         celltype_class = factor(celltype_class , c('Neuron', 'Glia')))
+df = df[colnames(z_clean), ]
+
+##################################################################
+# 2) grab the DEGs split by the DEGs to plot in neurons or glia
+res = here(DATADIR, 'rdas', 'OUD_Striatum_voom_limma_bigModelSVA_N22.celltype.rds') %>% readRDS()
+path = here('figures/explanatory/figure3_the_neuron_degs_and_cell_states',
+            'tables','s3.2_table_numDEG_overlap_bycelltype.xlsx') 
+res_overlap_list = path %>% excel_sheets() %>% set_names() %>% 
+  map(read_excel, path = path) %>% lapply(function(x) x %>% filter(numCelltype >=2) )
+sapply(res_overlap_list, nrow)
+
+#############################################################
+# 3) set up the heatmap plot parameters ahead of time
+alpha = 0.05
+gp_title = gpar(fontsize = 6, fontface = 'bold'); gp_label = gpar(fontsize = 4)
+u0 = unit(0, "mm"); u2 = unit(2, "mm"); u3 = unit(3, "mm")
+pal <- RColorBrewer::brewer.pal(11, 'PiYG')
+
+row_params = list(title_gp = gp_title, labels_gp = gp_label, 
+                  column_gap = u2, grid_height = u2, grid_width = u3, 
+                  legend_height = u3)
+column_params = list(title_gp = gp_title, labels_gp = gp_label, ncol = 1, 
+                     gap = unit(0, "cm"), row_gap = u0, column_gap = u0, 
+                     grid_height = u2, grid_width = u2)
+
+## make the legends
+lgd_zsc = Legend(col_fun = circlize::colorRamp2(seq(-4, 4,8/10 ), pal), 
+                 title = "zscore", labels_gp = gp_label, gap = unit(0, "cm"), 
+                 row_gap = u0, column_gap = u0, title_gp = gp_title,
+                 grid_height = u2, grid_width = u2, legend_height = unit(6, 'mm'))
+lgd_sex = Legend(labels = c('F', 'M'), title = "Sex", labels_gp = gp_label, 
+                 legend_gp = gpar(fill =c('F' = 'black', 'M' = 'yellow')),
+                 gap = unit(0, "cm"), row_gap = u0, column_gap = u0, legend_height = unit(6, 'mm'),
+                 grid_height = u2, grid_width = u2, title_gp = gp_title)
+lgd_oud = Legend(labels = names(dx_col), title = "OUD Dx", legend_height = unit(6, 'mm'),
+                 legend_gp = gpar(fill = dx_col), labels_gp = gp_label,
+                 gap = unit(0, "cm"), row_gap = u0, column_gap = u0, 
+                 grid_height = u2, grid_width = u2, title_gp = gp_title)
+
+
+########################################################
+## 4) plot all the glial DEGs w/ cell type overlaps
+df2 = df %>% arrange(celltype3, DSM.IV.OUD, Sex) %>% filter(celltype_class == 'Glia')
+
+## cell type legend
+lgd_cell = Legend(labels = names(typecolors)[c(10:12,14:15)], title = "Cell type", 
+                  legend_gp = gpar(fill =  typecolors[c(10:12,14:15)]), labels_gp = gp_label,
+                  gap = unit(0, "cm"), row_gap = u0, column_gap = u0, legend_height = unit(6, 'mm'),
+                  grid_height = u2, grid_width = u2, title_gp = gp_title)
+lgd_list = packLegend(lgd_zsc, lgd_oud,  lgd_cell, lgd_sex, max_height = unit(80, 'mm'))
+
+## set up the column and row annotation plot parameters
+column_ha = HeatmapAnnotation(
+  'Cell type' = df2$celltype3, 'OUD Dx' = df2$DSM.IV.OUD, 'Sex' = df2$Sex,
+  col = list(`Cell type` =  typecolors[c(10:12,14:15)], `OUD Dx` = dx_col, 
+             `Sex` = c('F' = 'black', 'M' = 'yellow')),
+  simple_anno_size = u2, annotation_name_gp= gp_title,
+  annotation_legend_param = column_params, show_legend = c(F, F, F))
+
+## grab the genes to be plot on the heatmap
+to_plot3= res$Glia %>% filter(adj.P.Val.Between < alpha) %>% 
+  filter(abs(logFC) > 1) %>% pull(gene) %>% unique()
+mat3 = z_clean[to_plot3,rownames(df2)]
+
+## grab the genes enriched in the pathways
+pathways_genes = here(PLOTDIR,'tables', 'figure4_clustered_glia_gsea_pathway_network.xlsx') %>% 
+  readxl::read_xlsx() %>% pull(leadingEdge) %>% sapply(str_split, ',') %>% unlist() %>% unique()
+
+ind_mark = which(to_plot3 %in% pathways_genes)
+row_ha = rowAnnotation(foo = anno_mark(at = ind_mark, labels = to_plot3[ind_mark], 
+                                   link_width = unit(3, "mm"), labels_gp = gp_label))
+
+## make the plot
+plot_fn = here(PLOTDIR,'plots', 'figure4_celltypes_DEG_heatmap.Glia.pdf')
+pdf(plot_fn, height = 70/in2mm, width = 121/in2mm, onefile = T)
+ht2 = Heatmap(mat3, col = pal,name = "zscore", cluster_columns = T, 
+        column_dend_height = u3, row_dend_width = u3,
+        show_column_names = F, show_row_names = F,
+        show_heatmap_legend = F, right_annotation = row_ha,
+        top_annotation = column_ha, heatmap_legend_param = row_params)
+
+draw(ht2, ht_gap = u0, annotation_legend_list = lgd_list)
+
+dev.off()
+
+
+
