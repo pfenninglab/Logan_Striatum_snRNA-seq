7b3af1466997071671d88f25d26703def56d965f
badoi <badoi@me.com>
2023-04-05 22:31:10 -0400

    add TF-gene module subpanels

---
+++ figures/explanatory/figure2_the_glia_degs_and_cell_states/figure2_TF_module_network_plot.R
@@ -0,0 +1,124 @@
+ss <- function(x, pattern, slot = 1, ...) { 
+  sapply(strsplit(x = x, split = pattern, ...), '[', slot) }
+options(stringsAsFactors = F)
+
+## packages for data table processing/plotting
+library(rcartocolor)
+library(data.table)
+library(tidyverse)
+library(here)
+library(readxl)
+library(wesanderson)
+library(cividis) #  devtools::install_github("marcosci/cividis")
+library(igraph)
+
+DATADIR='data/tidy_data/differential_expression_analysis'
+
+## make for this subdirs
+PLOTDIR='figures/explanatory/figure2_the_glia_degs_and_cell_states'
+here(PLOTDIR, c('plots', 'tables', 'rdas')) %>% sapply(dir.create, showWarnings = F)
+
+###################################
+# 0) pre-set plotting aesthetics
+subtypes = c('D1-Matrix', 'D2-Matrix',  'D1-Striosome', 'D2-Striosome','D1/D2-Hybrid')
+othertypes = c('Int-CCK', 'Int-PTHLH','Int-SST', 'Int-TH', 'All', 'Neuron', 'Glia',
+               'Astrocytes', 'Endothelial', 'Microglia', 'Mural', 'Oligos', 
+               'Oligos_Pre', 'Interneuron')
+glia_types = c('Astrocytes','Endothelial', 'Microglia', 'Oligos', 'Oligos_Pre')
+
+in2mm<-25.4
+my_theme = theme_classic(base_size = 5)
+alpha = 0.05
+
+sign_col = c('pink', 'lightgreen')
+sign_col2 = c('darkred', 'darkgreen')
+
+##############################################
+# 1) read in the cell type OUD DEGs
+rdasDir =file.path(DATADIR, 'rdas')
+save_res_fn = here(rdasDir, 'OUD_Striatum_voom_limma_bigModelSVA_N22.celltype.rds')
+res = readRDS(save_res_fn)[c('Glia', 'All', glia_types)]
+
+# number of cell types
+length(res) 
+
+# number of unique genes that are DEGs across any cell type
+deg_df = res %>% rbindlist()
+deg_signif_df = deg_df %>% filter(adj.P.Val.Between < alpha)
+max_gene_expr = deg_df %>% arrange(desc(AveExpr)) %>% 
+  distinct(gene, .keep_all = T) %>% dplyr::select(gene, AveExpr) %>% deframe()
+deg_signif_sign = deg_signif_df %>% group_by(gene) %>% 
+  summarise(sign = sign(mean(logFC))/2 + 1.5) %>% deframe()
+deg_sign = deg_df %>% group_by(gene) %>% 
+  summarise(sign = sign(mean(logFC))/2 + 1.5) %>% deframe()
+
+#############################################
+# 2) ead in the saved pseudo bulk TF regulon output 
+stats_group  = here('figures/exploratory/AUCell_gene_set_activities', 'tables', 'OUD_Striatum_pyscenic_differential_TF_modules.byCelltype.xlsx') %>% 
+  readxl::read_xlsx() %>% filter(celltype3 %in% glia_types ) %>% 
+  mutate(isSignif = p.adj < alpha )
+
+signif_TF = stats_group %>% filter(isSignif ) %>% pull(TF) %>% unique()
+signif_cell = stats_group %>% filter(isSignif ) %>% pull(celltype3) %>% unique()
+
+## reorder the TFs by the difference in OUD
+stats_group2 = stats_group %>% filter(TF %in% signif_TF, celltype3 %in% signif_cell) %>% 
+  group_by(TF) %>% mutate(tmp = mean(statistic)) %>% ungroup() %>% 
+  arrange(tmp) %>% mutate(TF = factor(TF, unique(TF)))
+
+TF_sign= stats_group %>% filter(isSignif ) %>% group_by(TF) %>% 
+  summarise(sign = sign(mean(estimate))/2 + 1.5) %>% deframe()
+  
+############################################
+# 2) read in the TF-gene relationships
+links = here('figures/exploratory/AUCell_gene_set_activities', 'tables', 'OUD_Striatum_pyscenic_detected_TF_modules.xlsx') %>% 
+  readxl::read_xlsx() %>%
+  dplyr::rename('TF' = 'V1', "gene" = "TargetGenes", 'edge.width' = 'importance') %>% 
+  filter(TF %in% signif_TF) 
+
+select_TF = c("NFKB1", 'FOXP2', 'RXRG', 'CEBPB')
+
+for(plot_TF in select_TF){
+  set.seed(0)
+  links2 = links %>% filter(TF %in% plot_TF) %>% 
+    mutate(isDEG = gene %in% deg_signif_df$gene, 
+           AveExpr = max_gene_expr[gene], 
+           edge.width = edge.width**2) %>% 
+    filter(edge.width > 1, AveExpr > 1) %>% 
+    arrange(edge.width, AveExpr) %>% 
+    top_n(50, edge.width)
+  
+  num_edges = nrow(links2)
+  links2 %>% dplyr::count(isDEG)%>% print()
+  
+  nodes = links2 %>% dplyr::select(c(gene, TF, isDEG)) %>% 
+    pivot_longer(cols = c(gene, TF), values_to = 'gene', names_to = 'type') %>% 
+    mutate(type = type =='TF', 
+           frame.color = ifelse(type, 'black', '#00000000'), 
+           color = ifelse(type, sign_col2[TF_sign[gene]], 
+                          ifelse(isDEG, sign_col2[deg_signif_sign[gene]], 
+                                 sign_col[deg_sign[gene]])), 
+           shape = 'rectangle', 
+           size = 8*str_count(gene), 
+           size2 = 15) %>% 
+    relocate('gene', .before = everything()) %>% 
+    arrange(desc(type)) %>% 
+    distinct(gene, .keep_all = T)
+  
+  net <- graph_from_data_frame(d=links2, vertices=nodes, directed=T)
+  V(net)$label.cex = .3
+  V(net)$label.color = 'black'
+  is_bipartite(net) #True
+  
+  ## make the plot of the gene modules
+  plot_fn = here(PLOTDIR,'plots', paste0('figure2_TF-gene_modules_',plot_TF,'.pdf'))
+  pdf(plot_fn, height = log10(num_edges)*30/in2mm, 
+      width = log10(num_edges)*30/in2mm, onefile = T)
+  par(mar=c(0,0,0,0))
+  # plot(net, edge.arrow.size=.1, edge.curved=.1, label.family = 'Helvetica', 
+  #      layout=layout_with_lgl(net))
+  plot(net, edge.arrow.size=.1, edge.curved=.1, label.family = 'Helvetica', 
+       layout=layout.fruchterman.reingold(net))
+  dev.off()
+}
+
---
+++ figures/explanatory/figure3_the_neuron_degs_and_cell_states/figure3_plot_TF_regulons_neuron.R
@@ -0,0 +1,81 @@
+ss <- function(x, pattern, slot = 1, ...) { 
+  sapply(strsplit(x = x, split = pattern, ...), '[', slot) }
+options(stringsAsFactors = F)
+
+## packages for data table processing/plotting
+library(SingleCellExperiment)
+library(ComplexHeatmap)
+library(rcartocolor)
+library(data.table)
+library(tidyverse)
+library(ggsci)
+library(here)
+library(readxl)
+library(ggh4x)
+library(wesanderson)
+library(cividis) #  devtools::install_github("marcosci/cividis")
+
+
+DATADIR='data/tidy_data/differential_expression_analysis'
+
+## make for this subdirs
+PLOTDIR='figures/explanatory/figure3_the_neuron_degs_and_cell_states'
+here(PLOTDIR, c('plots', 'tables', 'rdas')) %>% sapply(dir.create, showWarnings = F)
+
+###################################
+# 0) pre-set plotting aesthetics
+subtypes = c('D1-Matrix', 'D2-Matrix',  'D1-Striosome', 'D2-Striosome','D1/D2-Hybrid')
+subtypes_col = c('#1f78b4', '#a6cee3', '#e31a1c', '#fb9a99',  '#6a3d9a')
+names(subtypes_col) = subtypes %>% make.names()
+
+othertypes = c('Int-CCK', 'Int-PTHLH','Int-SST', 'Int-TH', 
+               'All', 'Neuron', 'Glia',
+               'Astrocytes', 'Endothelial', 'Microglia', 'Mural', 'Oligos', 
+               'Oligos_Pre', 'Interneuron')
+othertypes_col = c(carto_pal(4, "Safe"), 
+                   mypal = pal_npg('nrc')(3),
+                   carto_pal(length(othertypes) -7 , "Vivid"))
+names(othertypes_col) = othertypes
+othertypes_col = othertypes_col[-c(1:4)]
+typecolors = c(othertypes_col[1:3], subtypes_col, othertypes_col[c(10, 4:9)])
+
+dx_col = setNames(pal_npg(palette = c("nrc"), alpha = 1)(2), c('CTL', 'OUD'))
+
+in2mm<-25.4
+my_theme = theme_classic(base_size = 6)
+
+#############################################
+# 1) load in the pseudobulk of TF regulators
+alpha = 0.05
+
+## read in the saved pseudo bulk TF regulon output 
+save_pb_fn = here('figures/exploratory/AUCell_gene_set_activities', 'rdas',
+                  'OUD_Striatum_pyscenicl_TF_modules.pseudoBulkByCelltype.rds')
+aucell_pb = readRDS(save_pb_fn)
+
+stats_group  = here('figures/exploratory/AUCell_gene_set_activities', 'tables', 'OUD_Striatum_pyscenic_differential_TF_modules.byCelltype.xlsx') %>% 
+  readxl::read_xlsx() %>% filter(grepl('^D|Int', celltype3) ) %>% 
+  mutate(isSignif = p.adj < alpha )
+
+signif_TF = stats_group %>% filter(isSignif ) %>% pull(TF) %>% unique()
+signif_cell = stats_group %>% filter(isSignif ) %>% pull(celltype3) %>% unique()
+
+## reorder the TFs by the difference in OUD
+stats_group2 = stats_group %>% filter(TF %in% signif_TF, celltype3 %in% signif_cell) %>% 
+  group_by(TF) %>% mutate(tmp = mean(statistic)) %>% ungroup() %>% 
+  arrange(tmp) %>% mutate(TF = factor(TF, unique(TF)))
+
+#############################################
+# 2) load in the pseudobulk of TF regulators
+plot_tf_enrichment_fn = here(PLOTDIR, 'plots', 'figure3_TF_regulon_neuron.heatmap.pdf')
+pdf(plot_tf_enrichment_fn, height = 80/in2mm, width = 59/in2mm)
+ggplot(stats_group2, aes(x = celltype3, y = TF, fill = statistic)) + 
+  geom_tile(aes(color = isSignif), linewidth = .5, width = .95, height = .9) + 
+  scale_fill_distiller(palette = "PiYG", direction = 1, 't-statistic')+
+  scale_color_manual(values = c('#00000000', 'black'), 'FDR < 0.05')+
+  guides(color = guide_legend(override.aes = list(fill = "white"))) +
+  my_theme + xlab('Cell type') + 
+  theme(axis.text.x = element_text(angle = 30, vjust = 1, hjust=1)) +
+  ylab('Differential TF-gene regulatory module in OUD')
+dev.off()
+
